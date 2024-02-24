6ba848f9698bf533b29e1e1b756db62468a28372
Alexandre <alexandre.laborde@gmail.com>
2024-02-24 17:40:47 -0500

    Update - Nav Bar mobile

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,15 @@ export default function Home() {
   return (
     <>
       {/* Hero */}
-      <div className=" flex flex-col lg:gap-3 lg:ml-20  p-4 lg:h-screen overflow-hidden">
+      <div className=" flex flex-col gap-3 md:ml-20  p-4  overflow-hidden">
         {/* Bloc - info */}
-        <div className=" flex flex-col lg:flex-row lg:gap-4">
+        <div className=" flex flex-col md:flex-row gap-4">
           {/* Bloc - Title */}
-          <div className="  flex items-end justify-center flex-col gap-4 rounded-lg border-4 border-neutral // basis-1/2 lg:basis-3/4 2xl:basis-4/5 // px-3 md:px-8 lg:px-10 xl:px-8  2xl:px-16  ">
+          <div className="flex items-end xl:items-start justify-center flex-col gap-4 rounded-lg border-4 border-neutral // md:basis-2/3 lg:basis-3/4 2xl:basis-4/5 // px-4 md:px-4 lg:px-8 xl:px-8  2xl:px-8 py-8 md:py-0  ">
             {/* Title - texte */}
-            <div className="w-full flex flex-col justify-start lg:py-4 gap-8 ">
+            <div className="flex flex-col justify-center gap-8">
               <Title />
-              <p className="text-lg xl:text-2xl font-light text-neutral">
+              <p className="text-lg xl:text-2xl font-light text-neutral pr-8 xl:pr-0">
                 Bienvenue dans mon espace de croissance et
                 <span className="inline-block bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent font-bold text-xl xl:text-4xl">
                   d’innovation
@@ -29,11 +29,11 @@ export default function Home() {
             </div>
           </div>
           {/* Bloc - slider */}
-          <div className="lg:w-0 md:basis-1/2 lg:basis-1/4 2xl:basis-1/5 flex justify-center items-center  bg-white rounded-lg // ">
+          <div className="w-[50%] md:w-0 md:basis-1/3 lg:basis-1/4 2xl:basis-1/5 flex justify-center items-center  bg-white rounded-lg // ">
             <SwiperHome></SwiperHome>
           </div>
         </div>
-        <div className="flex border-4 border-neutral rounded-lg bg-origin-border">
+        <div className=" flex border-4 border-neutral rounded-lg bg-origin-border">
           <Slogan />
         </div>
       </div>
