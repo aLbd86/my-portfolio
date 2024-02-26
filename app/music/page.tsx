4bdeeb85cffd7bd6e31ca62b7576599051922924
Alexandre <alexandre.laborde@gmail.com>
2024-02-26 14:24:26 -0500

    Update video et audio

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -11,15 +11,18 @@ export default function page() {
           <h1 className="flex text-3xl text-neutral font-bold ">Espace musical</h1>
         </div>
         {/* Media Player */}
-        <div className="flex flex-col py-4 items-center">
-          <div className="flex lg:basis-3/5">
+        <div className="flex flex-col md:flex-row py-4 items-center gap-8">
+          <div className="flex lg:basis-1/3">
             <Player />
           </div>
           {/* Contenu text */}
-          <div className="flex p-4 lg:basis-2/5">
+          <div className="flex p-4 lg:basis-2/3 border-4 border-neutral rounded-lg">
             <h3 className="flex">Toutes ces pièces musicales ont une histoire</h3>
             <p className=""></p>
           </div>
+        </div>
+        <div className="border-4 border-neutral rounded-lg">
+          <video controls src="./video/fou-bar-720.mp4" className="w-[50%]"></video>
         </div>
       </main>
     </>
