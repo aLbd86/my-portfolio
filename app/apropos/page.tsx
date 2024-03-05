343945966bb7182012c30b731107530116d17b81
Alexandre <alexandre.laborde@gmail.com>
2024-03-05 18:23:40 -0500

    test4

--- app/apropos/page.tsx
+++ app/apropos/page.tsx
@@ -1,8 +1,8 @@
 export default function page() {
   return (
     <>
-      <div className="border-neutral flex items-center justify-center rounded-lg border-4 py-4">
-        <h1 className="text-neutral flex pt-12 text-3xl font-bold ">Qui suis-je?</h1>
+      <div className="border-neutral flex pt-12 items-center justify-center rounded-lg border-4 py-4">
+        <h1 className="text-neutral flex  text-3xl font-bold ">Qui suis-je?</h1>
       </div>
     </>
   );
