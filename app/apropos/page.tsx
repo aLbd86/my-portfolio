5f61f04e266fdd615f3e0e1dffb41cfba5563161
Alexandre <alexandre.laborde@gmail.com>
2024-03-05 17:36:25 -0500

    test envoi

--- app/apropos/page.tsx
+++ app/apropos/page.tsx
@@ -2,7 +2,9 @@ export default function page() {
   return (
     <>
       <div className="border-neutral flex items-center justify-center rounded-lg border-4 py-4">
-        <h1 className="text-neutral flex text-3xl font-bold ">À propos</h1>
+        <h1 className="text-neutral flex pt-12 text-3xl font-bold ">
+          À propos
+        </h1>
       </div>
     </>
   );
