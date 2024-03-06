54f03da301b96ff23c4d569533ed9cfae19c3972
Alexandre <alexandre.laborde@gmail.com>
2024-03-05 22:07:45 -0500

    the last of the day

--- app/apropos/page.tsx
+++ app/apropos/page.tsx
@@ -3,11 +3,9 @@ export default function page() {
     <>
       <div className="flex flex-col items-center justify-center p-4 md:ml-20">
         <div className="border-neutral flex w-full items-center justify-center rounded-lg border-4 py-4">
-          <h1 className="text-neutral flex  text-3xl font-bold ">
-            Qui suis-je?
-          </h1>
+          <h1 className="text-neutral flex  text-3xl font-bold ">Qui suis-je?</h1>
         </div>
-        <div className="flex text-xl font-light">
+        <div className="flex py-6 text-xl font-light">
           <p>En construction</p>
         </div>
       </div>
