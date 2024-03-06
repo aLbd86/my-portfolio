f6eaded34067add3902686e2d7e8930fad32ab0a
Alexandre <alexandre.laborde@gmail.com>
2024-03-05 22:41:55 -0500

    je lache pas

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,13 @@
-export const apps = [
-  {
-    name: "portfolio",
-    script: "npm",
-    args: "run start -- --port 3000",
-    cwd: "/var/www/portfolio",
-    autorestart: true,
-    watch: false,
-    max_memory_restart: "300M",
-  },
-];
+module.exports = {
+  app: [
+    {
+      name: "portfolio",
+      script: "npm",
+      args: "run start -- --port 3000",
+      cwd: "/var/www/portfolio",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "300M",
+    },
+  ],
+};
